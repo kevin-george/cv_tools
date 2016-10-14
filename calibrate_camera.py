9eb3b595d23eda94fbb17308665d19d3196c6ae8
Kevin George <kevin.george@protonmail.com>
2016-10-14 15:38:54 -0400

    Added cornerSubPix for better acccuracy

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -8,6 +8,8 @@ import cv2
 import numpy as np
 
 def calibrate(images):
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
     #Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((7*9,3), np.float32)
     objp[:,:2] = np.mgrid[0:9,0:7].T.reshape(-1,2)
@@ -16,19 +18,27 @@ def calibrate(images):
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
 
-    for fname in images:
-        #Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(fname, (9,7), None)
+    for entry in images:
+        color = images[entry]
+        gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
 
-        #Draw and display the corners
-        #cv2.drawChessboardCorners(img, (9,7), corners, ret)
+        #Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,7), None)
 
         #If found, add object points, image points (after refining them)
         if ret:
             objpoints.append(objp)
+
+            cv2.cornerSubPix(gray,corners,(4,4),(-1,-1),criteria)
             imgpoints.append(corners)
 
-    image_size = images[0].shape[::-1]
+            #Draw and display the corners
+            cv2.drawChessboardCorners(color, (9,7), corners, ret)
+            cv2.imshow(entry, color)
+            cv2.waitKey(0)
+
+
+    image_size = gray.shape[::-1]
     reproj_err, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints,
                                                               imgpoints,
                                                               image_size,
